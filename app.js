dc103816dc845afd2d35fd2e5f2dee0c5818805f
Bruno <devbrunocode@gmail.com>
2024-09-06 20:19:32 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+function pesquisar() {
+    // Seleciona a seção onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("card-resultado");
+  
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    if (campoPesquisa == "") {
+      section.innerHTML = "<p>Você não digitou o nome de um personagem.</p>"
+      return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa
+    let resultados = "";
+    let animal = "";
+    let especie = "";
+  
+    // Itera info cada dado na lista de dados
+    for (let dado of dados) {
+      animal = dado.animal.toLowerCase()
+      especie = dado.especie.toLowerCase()
+
+      if (animal.includes(campoPesquisa) || especie.includes(campoPesquisa)) {
+        // Constrói o HTML para cada item do resultado da pesquisa, incluindo o nome do personagem, descrição e link
+        resultados += `
+        <div class="card-animal">    
+                <img src=${dado.imagem} alt="">            
+                <div class="info">
+                    <h2>${dado.animal}</h2>
+                    <h3>Espécie: ${dado.especie}</h3>
+                    <h3>Curiosidade: ${dado.curiosidade}</h3>                    
+                </div>                           
+            </div>            
+            <div class="info-descricao">
+                <p>${dado.info}</p>
+                <a href="${dado.info} target="_blank">Clique aqui e saiba mais!</a>  
+            </div>
+      `;
+      }        
+    }
+
+    if (!resultados) {
+      resultados = "<p> O personagem escolhido ainda não foi adicionado.</p>"
+    }
+
+    // Atualiza o conteúdo HTML da seção com os resultados construídos
+    section.innerHTML = resultados;
+  }
---
+++ dados.js
@@ -0,0 +1,72 @@
+let dados = [
+    {
+        imagem:"https://images.unsplash.com/photo-1552486274-011c96d67c54?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        animal: "Arara-azul",
+        especie: "Aves",
+        curiosidade: "Elas vivem em grupos e se comunicam através de gritos e assobios, como se estivessem conversando.",
+        info: "Imagine um pássaro gigante, com penas azuis bem brilhantes, quase do tamanho de um bebê! Essa é a arara-azul, uma ave muito bonita e famosa do Brasil. Ela mora nas grandes árvores da floresta, e adora comer frutas e sementes. As araras-azuis são muito inteligentes e vivem em grupos, sempre se ajudando e brincando juntas. Mas é importante lembrar que elas estão em perigo, por isso precisamos cuidar da natureza para que elas possam continuar voando livres e felizes!",
+        link: "https://pt.wikipedia.org/wiki/Arara-azul-grande",
+    },{
+        imagem:"https://images.unsplash.com/photo-1552761047-68f241b7aef4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        animal: "Tucano",
+        especie: "Aves",
+        curiosidade: "Quando está muito calor, ele coloca o bico na sombra e a água que bebe fica mais fresquinha!",
+        info: "Imagina um pássaro com um bico enorme e colorido! Esse é o tucano! O bico dele é bem chamativo, mas não se preocupe, ele não é pesado e nem serve para morder. O tucano usa o bico para pegar frutas e sementes que estão bem escondidas nas árvores. Ele é como um papagaio, mas com um bico muito mais legal! O tucano é bem sociável e vive em grupos, sempre fazendo muito barulho e alegria pela floresta!",
+        link: "https://pt.wikipedia.org/wiki/Tucano",
+    },{
+        imagem:"https://images.unsplash.com/photo-1671144595957-f25ef7674145?q=80&w=2067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        animal: "Carcará",
+        especie: "Aves",
+        curiosidade: "Ele faz um som que parece com o próprio nome dele! É como se ele estivesse se apresentando por aí!",
+        info: "Ele tem uma cara bem engraçada, com uma crista de penas na cabeça que parece um chapéu! O carcará adora comer carne, mas também gosta de frutas e até mesmo de insetos. Ele é muito importante para o nosso ecossistema, pois ajuda a manter a natureza limpa e saudável. O carcará é um símbolo do nosso sertão e faz parte da nossa cultura!",
+        link: "https://pt.wikipedia.org/wiki/Carcar%C3%A1",
+    },{
+        imagem:"https://images.unsplash.com/photo-1569277051793-783398dc36ff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        animal: "Onça-pintada",
+        especie: "Mamíferos",
+        curiosidade: "Cada onça-pintada tem suas manchas únicas, como se fossem suas próprias digitais. Assim, é como se cada onça fosse uma obra de arte da natureza!",
+        info: "Imagine um gato gigante, com manchas pretas lindas e um olhar poderoso! Essa é a onça-pintada, o maior felino das Américas e o nosso super herói brasileiro! Ela mora nas florestas e é uma caçadora incrível. A onça-pintada é muito forte e ágil, e adora nadar. Mas cuidado, ela é um animal selvagem e precisa de muito espaço para viver. Por isso, precisamos cuidar da natureza para que ela possa continuar a proteger a floresta!",
+        link: "https://pt.wikipedia.org/wiki/Panthera_onca",
+    },{
+        imagem:"https://images.unsplash.com/photo-1598315805844-d15164294b16?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        animal: "Capivara",
+        especie: "Mamíferos",
+        curiosidade: "Imagine um porquinho gigante, todo molhado e feliz! Essa é a capivara, o maior roedor do mundo e uma brasileira nata! Ela adora ficar perto da água, tomando sol e nadando feito um peixe. A capivara é muito tranquila e vive em grupos grandes, como se fosse uma família enorme. Ela se alimenta de plantinhas e grama, e é super amiga de outras animais! Mas cuidado para não confundir a capivara com um porquinho da índia, viu? Ela é muito maior e mais pesada!",
+        link: "https://pt.wikipedia.org/wiki/Capivara",
+    },{
+        imagem:"https://images.unsplash.com/photo-1558724483-0a9d17b1defc?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        animal: "Mico-leão-dourado",
+        especie: "Mamíferos",
+        curiosidade: "Eles vivem em grupos e cuidam uns dos outros. Os filhotinhos são muito pequenos quando nascem e precisam da ajuda de todos para crescerem fortes e saudáveis!",
+        info: "Imagine um macaquinho pequenininho, com um pelo dourado brilhante e olhos bem espertos! Esse é o mico-leão-dourado, um animalzinho muito especial e que só vive nas nossas matas brasileiras. Ele é tão pequeno que cabe na palma da sua mão! O mico-leão-dourado adora pular de galho em galho e brincar com seus amigos. Mas ele está um pouco triste, porque suas casas, as florestas, estão ficando cada vez menores. Por isso, precisamos cuidar bem da natureza para que ele possa continuar pulando e brincando feliz!",
+        link: "https://pt.wikipedia.org/wiki/Mico-le%C3%A3o-dourado",
+    },{
+        imagem:"https://en.wikipedia.org/wiki/Amazon_river_dolphin#/media/File:Inia_geoffrensis_179341177.jpg",
+        animal: "Boto-cor-de-rosa",
+        especie: "Mamíferos",
+        curiosidade: "Quando são filhotes, eles são mais cinza. A cor rosa vai aparecendo conforme eles crescem! Quanto mais velho o boto, mais rosado ele fica.",
+        info: "Imagina um golfinho que mora nos rios e tem a pele rosada? Esse é o boto-cor-de-rosa! Ele é bem grandinho e muito inteligente. O boto-cor-de-rosa adora brincar na água e é super curioso. Ele vive nos rios da Amazônia e é um animal muito importante para o nosso ecossistema. As crianças da Amazônia têm muitas histórias info o boto-cor-de-rosa, que dizem que ele se transforma em um homem bonito para encontrar as pessoas!",
+        link: "https://pt.wikipedia.org/wiki/Boto-cor-de-rosa",
+    },{
+        imagem:"https://pt.wikipedia.org/wiki/Tartaruga-de-pente#/media/Ficheiro:Tortue_imbriqueeld4.jpg",
+        animal: "Tartaruga-de-pente",
+        especie: "Répteis",
+        curiosidade: "Ela cava um buraco na areia da praia e esconde os ovos lá dentro. Depois, ela tapa tudo direitinho para proteger os filhotes do sol e de outros animais. É como se ela estivesse construindo uma casa para os seus bebês!",
+        info: "Ela é uma tartaruga marinha muito especial! Imagina um casco marrom e amarelo, com placas que parecem um pente de cabelo. Por isso o nome dela! A tartaruga-de-pente gosta de nadar em águas quentinhas e vive perto dos corais. Ela é uma nadadora muito rápida e elegante, deslizando pela água como se estivesse dançando. Mas, infelizmente, ela está em perigo por causa da poluição e da pesca. Precisamos cuidar dela para que ela continue nadando nos nossos oceanos!",
+        link: "https://pt.wikipedia.org/wiki/Tartaruga-de-pente",
+    },{
+        imagem:"https://www.pescazila.com.br/storage/2022/03/46.Baiacu.png",
+        animal: "Baiacu",
+        especie: "Peixes",
+        curiosidade: "Tem um veneno super forte. Por isso, a gente nunca deve tocar em um baiacu, mesmo que ele esteja todo inflado",
+        info: "Já viu um peixe que parece um balão? Esse é o baiacu! Quando se sente ameaçado, ele enche o corpo de ar e fica todo redondinho e cheio de espinhos. Assim, fica muito difícil para outros animais o comerem. É como se ele estivesse dizendo: 'Sai pra lá, eu sou perigoso!'.O baiacu é bem colorido e tem um bico forte para mastigar algas e outros alimentos.",
+        link: "https://pt.wikipedia.org/wiki/Baiacu",
+    },{
+        imagem:"https://images.unsplash.com/photo-1571062953968-292bffd97201?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        animal: "Tamanduá-bandeira",
+        especie: "Mamíferos",
+        curiosidade: "Quando dorme, ele enrola a cauda em volta do corpo para se manter quentinho. É como se ele tivesse um cobertor peludo e listrado!",
+        info: "Imagina um animal bem grandão, com uma língua bem comprida e uma cauda que parece uma bandeira. Ele é um animal bem tranquilo e passa o dia todo comendo formigas e cupins. Para isso, ele usa sua língua pegajosa e a enfia nas formigueiras. O tamanduá-bandeira é um animal muito importante para a natureza, pois ajuda a controlar a população de insetos. Ele é como um aspirador de pó gigante, mas em vez de sugar poeira, ele suga formigas!",
+        link: "https://pt.wikipedia.org/wiki/Tamandu%C3%A1-bandeira",
+    }
+];
